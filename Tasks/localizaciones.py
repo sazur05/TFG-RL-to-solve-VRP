f78d1fe9e861ab94b039b03e5d28d4f5ddc3c52d
sazur05 <134415182+sazur05@users.noreply.github.com>
2024-02-29 18:54:59 +0100

    Update localizaciones.py

--- Tasks/localizaciones.py
+++ Tasks/localizaciones.py
@@ -2,34 +2,27 @@ import torch
 
 #Esta función tiene de entradas el input_size== num_nodo y el STATIC_SIZE == coordenadas
 #Con esta funcion estoy creando un tensor de dos dimensiones, donde se encuentra la cantidad de nodos (input_size) y las dimensiones de las coordenadas.
+#Así podremos obtener las coordenadas de los clientes para poder visualizarlas cuando se inicie el proceso de entrenamiento o de test
 
 
 def generador_coordenadas_clientes(input_size, STATIC_SIZE):
     clientes = torch.rand((STATIC_SIZE, input_size))  # clientes
     deposito = torch.tensor([[0], [0]])  # depósito
-    #deposito2 = torch.tensor([[0], [1]])
-    localizaciones = torch.cat((deposito, clientes), dim=1) ##localizaciones = torch.cat( deposito1, deposito2, clientes), dim=2)
+    localizaciones = torch.cat((deposito, clientes), dim=1)
     print(localizaciones)
     return localizaciones
 
  # Ahora debemos crear un tensor que acumule todas las instancias deseadas del problema con nustras posiciones fijas
-
-def generador_static_entrenamiento(num_samples, STATIC_SIZE, input_size):
-    deposito = torch.tensor([[0], [0]])
-    #deposito2 = torch.tensor([[0], [1]])
-    clientes = torch.rand(num_samples, STATIC_SIZE, input_size)
-    #Debemos expandir el tensor depósito a las dimensiones del tensor cliente, es decir, añadir dos dimensiones al depósito, para luego poder concatenar ambos tensores
-    locations = torch.cat((deposito.expand(num_samples, -1, -1), clientes), dim=2) #locations = torch.cat((deposito.expand(num_samples, -1, -1), deposito2.expand(num_samples, -1, -1), clientes), dim=2)
-    return locations
-
-
 def generador_static_validación(num_samples, localizaciones):
     locations_repetidas = localizaciones.expand(num_samples, -1, -1)
     return locations_repetidas
-###Con estas dos funciones podremos crear unas localizaciones fijas de unos clientes que se repitan tanto en las instancias de entrenamiento, como de validación, como de test.
 
-### 3er cambio: 2 depósitos en el problema, al igual que en el caso anterior debemos determinar también cuál será la posición determinada de nuestro depósito.
-### Por otro lado, también deberemos cambiar la dimensionalidad de locations1 = torch.rand((input_size-1, STATIC_SIZE)) --> locations1 = torch.rand((input_size-2, STATIC_SIZE))
-
+##En el tensor de entrenamiento las posiciones de los clientes, para cada instancia del problema, deben ser distintas para que el agente aprenda de manera más eficaz y con un número mayor de problemas con distinto entorno
+def generador_static_entrenamiento(num_samples, STATIC_SIZE, input_size):
+    deposito = torch.tensor([[0], [0]])
+    clientes = torch.rand(num_samples, STATIC_SIZE, input_size)
+    #Debemos expandir el tensor depósito a las dimensiones del tensor cliente, es decir, añadir dos dimensiones al depósito, para luego poder concatenar ambos tensores
+    locations = torch.cat((deposito.expand(num_samples, -1, -1), clientes), dim=2)
+    return locations
 locations = generador_static_entrenamiento(10,2,10)
 print(locations.shape)
